4d05c835367c35e64071337a7670a4461dbaf53b
Grégory Paul <me@gpaul.me>
2021-10-20 21:17:05 +0200

    Customize admin interface to allow request edition

--- repaircafeapp/admin.py
+++ repaircafeapp/admin.py
@@ -1,5 +1,17 @@
+from django.utils.html import format_html
 from django.contrib import admin
-
+from django.urls import reverse
 from .models import Request
 
-admin.site.register(Request)
+
+class RequestAdmin(admin.ModelAdmin):
+    date_hierarchy = 'reparation_date'
+    list_display = ('reparation_date', 'name_text', 'firstname_text', 'locality_text',
+                    'category_text', 'brand_text', 'token_url')
+
+    def token_url(self, obj):
+        url = reverse('repaircafeapp:edit', kwargs={'token': obj.token_text})
+        return format_html("<a href='{url}' target='blank'>{url}</a>", url=url)
+
+
+admin.site.register(Request, RequestAdmin)
